b610b37640946db39eab448e501547497eac6d43
Octoping <53991994+Octoping925@users.noreply.github.com>
2022-11-22 08:17:20 +0900

    제목이 살짝 짤리는 현상 수정

--- layouts/layout.js
+++ layouts/layout.js
@@ -31,7 +31,7 @@ const Layout = ({
       fullWidth={fullWidth}
     >
       <article>
-        <h1 className="font-bold text-6xl flex-initial bg-gradient-to-r from-green-400 to-blue-500 bg-gradient-to-r text-transparent bg-clip-text">
+        <h1 className="font-bold text-6xl inline bg-gradient-to-r from-green-400 to-blue-500 bg-gradient-to-r text-transparent bg-clip-text">
           {frontMatter.title}
         </h1>
         {frontMatter.type[0] !== 'Page' && (
