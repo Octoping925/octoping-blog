204416239001e6cd6b041b66da8904ba1133e8b3
Octoping <53991994+Octoping925@users.noreply.github.com>
2022-11-22 13:21:45 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: 'https://og-image-korean.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: '',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
@@ -47,7 +47,7 @@ const BLOG = {
       distractionFreeMode: false
     },
     utterancesConfig: {
-      repo: ''
+      repo: 'Octoping925/octoping-blog'
     },
     cusdisConfig: {
       appId: '', // data-app-id
