dcb586b74f3e9835d1c71fe1286f52853ecbde81
Octoping <mooner925@gmail.com>
2023-03-12 14:17:41 +0900

    fix: usePagination 오류 수정

--- hooks/usePagination.ts
+++ hooks/usePagination.ts
@@ -1,19 +1,19 @@
 import BLOG from "@/blog.config";
 import { useEffect, useState } from "react";
 
-export function usePagination(currentPage: number, totalPage: number) {
-  const [pages, setPages] = useState([]);
+export function usePagination(currentPage: number, totalPosts: number) {
+  const [pages, setPages] = useState<number[]>([]);
   const displayPageCnt = BLOG.displayPageOfPagination;
 
   const grade = Math.floor(currentPage / displayPageCnt);
 
   const startPage = grade * displayPageCnt + 1;
-  const maxPage = Math.floor(totalPage / displayPageCnt);
+  const maxPage = Math.ceil(totalPosts / BLOG.postsPerPage);
   const endPage = Math.min(startPage + displayPageCnt - 1, maxPage);
 
   useEffect(() => {
     setPages([...Array(endPage - startPage + 1)].map((_, i) => startPage + i));
-  }, [totalPage, currentPage]);
+  }, [totalPosts, currentPage]);
 
   const isAbleToGoPrev = currentPage > 1;
   const isAbleToGoNext = currentPage < endPage;
