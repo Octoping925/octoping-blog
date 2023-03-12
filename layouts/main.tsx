aa8d1e8f824d15ba454e1863b0065343e5482c1c
Octoping <mooner925@gmail.com>
2023-03-12 15:25:57 +0900

    feat: Main의 레이아웃을 main.tsx로 추출, Main에 검색창 추가

---
+++ layouts/main.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import BlogPost from "@/components/BlogPost";
+import Container from "@/components/Container";
+import Tags from "@/components/Tags";
+import { Post } from "@/types";
+import Pagination from "@/components/Pagination";
+
+const MainLayout = ({
+  page,
+  postsToShow,
+  totalPosts,
+}: {
+  page: number;
+  postsToShow: Post[];
+  totalPosts: Post[];
+}) => {
+  const [searchValue, setSearchValue] = useState("");
+  let filteredBlogPosts = [];
+
+  if (searchValue !== "") {
+    filteredBlogPosts = totalPosts.filter((post) => {
+      const tagContent = post.tags ? post.tags.join(" ") : "";
+      const searchContent = post.title + post.summary + tagContent;
+      return searchContent.toLowerCase().includes(searchValue.toLowerCase());
+    });
+  } else {
+    filteredBlogPosts = postsToShow;
+  }
+
+  return (
+    <Container layout={undefined} fullWidth={false}>
+      <div className="relative">
+        <input
+          type="text"
+          placeholder="검색하기"
+          className="block w-full border px-4 py-2 border-black bg-white text-black dark:bg-night dark:border-white dark:text-white"
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+        <svg
+          className="absolute right-3 top-3 h-5 w-5 text-black dark:text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+      </div>
+      <div className="article-container my-8">
+        {!filteredBlogPosts.length && (
+          <p className="text-gray-500 dark:text-gray-300">
+            검색 결과가 없습니다.
+          </p>
+        )}
+        {filteredBlogPosts.slice(0, 20).map((post) => (
+          <BlogPost key={post.id} post={post} />
+        ))}
+      </div>
+      {searchValue || <Pagination page={page} totalPosts={totalPosts.length} />}
+    </Container>
+  );
+};
+
+export default MainLayout;
