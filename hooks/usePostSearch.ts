ccc410c3d1bcc11240dfb41c0203ee17b3862df7
Octoping <mooner925@gmail.com>
2023-03-12 17:33:46 +0900

    feat: usePostSearch 훅 추가

---
+++ hooks/usePostSearch.ts
@@ -0,0 +1,25 @@
+import { Post } from "@/types";
+import { useEffect, useState } from "react";
+
+export function usePostSearch(posts: Post[], searchValue: string) {
+  const [searchResults, setSearchResults] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const searchResult = searchAlgorithm(posts, searchValue);
+    setSearchResults(searchResult);
+  }, [posts, searchValue]);
+
+  function searchAlgorithm(posts: Post[], searchValue: string) {
+    if (searchValue === "") {
+      return posts;
+    }
+
+    return posts.filter((post) => {
+      const tagContent = post.tags ? post.tags.join(" ") : "";
+      const searchContent = post.title + post.summary + tagContent;
+      return searchContent.toLowerCase().includes(searchValue.toLowerCase());
+    });
+  }
+
+  return searchResults;
+}
